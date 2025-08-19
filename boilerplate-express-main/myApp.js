94c8ca07138b0ce2ea20bb82c2bec29dda36d854
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-19 08:51:06 +0300

    Add json route

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,6 +1,17 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+// serve static assets
+let path = __dirname + "/public";
+app.use("/public", express.static(path));
+
+// send index.html on root request
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
